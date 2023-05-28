7a199895bb8d00326b7ef33a6a2399f49843f307
Dorra <laouiti@viada.de>
2023-05-28 18:46:43 +0200

    attempt to deploy

--- src/app/services/sharedService/shared.service.ts
+++ src/app/services/sharedService/shared.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from "@angular/core";
+import { environment } from "../../../environments/environment.prod";
 
 @Injectable({
   providedIn: "root",
 })
 export class SharedService {
-  public baseUrl: string = "https://api.openweathermap.org";
+  public baseUrl: string = environment.apiUrl;  
   public api_ID: string = "5a4b2d457ecbef9eb2a71e480b947604";
   public picturesUrl: string =
     "https://www.angulartraining.com/images/weather/";
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://api.openweathermap.org',
+    debugMode: false,
+    // Other production-specific configurations
+  };
